6c5403e17cb95cd03c97f72d57c31d4aca56a0b6
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-03-23 15:16:25 +0200

    fixing controller

--- src/config.py
+++ src/config.py
@@ -1,6 +1,10 @@
+import logging
 import os
 import pathlib
+import sys
+
 from dotenv import load_dotenv
+from loguru import logger
 
 load_dotenv()
 ROOT_DIR = pathlib.Path(__file__).parent.parent
@@ -13,3 +17,23 @@ BAUDRATE = os.environ['BAUDRATE']
 # fastapi config
 APP_PORT = int(os.environ['APP_PORT'])
 APP_HOST = os.environ['APP_HOST']
+
+
+# logging
+logger.remove()
+logging.disable()
+LOGGING_FORMAT = (
+    "<level>{time:YYYY-MM-DD HH:mm:ss.SSS}</level> "
+    "<level>{level}</level>: "
+    "<level>{name}</level> - "
+    "<level>{message}</level>"
+)
+
+logger.add(
+    ROOT_DIR / "logs" / "log_{time:YYYY-MM-DD}.log",
+    format=LOGGING_FORMAT,
+    level="DEBUG",
+    mode="a",
+    retention="5 days",
+)
+logger.add(sys.stdout, format=LOGGING_FORMAT)
--- src/controllers.py
+++ src/controllers.py
@@ -2,12 +2,14 @@ import serial
 import time
 
 from src import config
+from src.config import logger
 
 
 class SmsController:
     com_port = serial.Serial(config.SIM_SOCKET, baudrate=config.BAUDRATE, timeout=1)
 
     def _send_at(self, command, delay=1):
+        logger.info(f'{command = }')
         self.com_port.write((command + "\r\n").encode())
         time.sleep(delay)
         response = self.com_port.read(self.com_port.inWaiting()).decode()
