af181b69010c377329e052e14588bcd3d2ea9a78
alexey_naidiuk <alexeynaidiuk@gmail.com>
2025-03-23 17:29:16 +0200

    added few operations

--- src/controllers.py
+++ src/controllers.py
@@ -1,5 +1,6 @@
-import serial
 import time
+
+import serial
 
 from src import config
 from src.config import logger
@@ -9,20 +10,26 @@ class SmsController:
     com_port = serial.Serial(config.SIM_SOCKET, baudrate=config.BAUDRATE, timeout=1)
 
     def __init__(self):
-        self._send_at('at+cmgf=1')
+        self._send_at("at+cmgf=1")
 
     def _send_at(self, command, delay=1):
-        logger.info(f'{command = }')
+        logger.info(f"{command = }")
         self.com_port.write((command + "\r\n").encode())
         time.sleep(delay)
         response = self.com_port.read(self.com_port.inWaiting()).decode()
         logger.info(response)
         return response
 
-    def get_messages(self, folder='all'):
+    def delete_specific_message(self, index: str | int):
+        return self._send_at(f"AT+CMGD={index}")
+
+    def get_specific_message(self, index: str | int):
+        return self._send_at(f"AT+CMGR={index}")
+
+    def get_messages(self, folder="all"):
         """
         at+cmgl="all”
 
         :return:
         """
-        return self._send_at(f'at+cmgl="{folder}"'.upper(), .5)
+        return self._send_at(f'at+cmgl="{folder}"'.upper(), 0.5)
--- src/main.py
+++ src/main.py
@@ -19,6 +19,15 @@ async def get_messages(folder="all"):
     return sms_controller.get_messages(folder)
 
 
+@app.get('/messages/{message_id}')
+async def get_specific_message(message_id: str | int):
+    return sms_controller.get_specific_message(message_id)
+
+
+@app.delete('/messages/{message_id}')
+async def delete_specific_message(message_id: str | int):
+    return sms_controller.delete_specific_message(message_id)
+
 def main():
     uvicorn.run("src.main:app", host=config.APP_HOST, port=config.APP_PORT)
 
